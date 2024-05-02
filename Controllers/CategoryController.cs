2d17eabc90d00545a27750bd6f46272d560e1f13
Omar <omar.m10@hotmail.com>
2024-05-02 18:24:49 +0200

    Add flow from model to view, including seeding from database

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+using Bulky_Mvc.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bulky_Mvc.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        var objCategoryList = _db.Categories.ToList();
+        return View(objCategoryList);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View("Error!");
+    }
+}
