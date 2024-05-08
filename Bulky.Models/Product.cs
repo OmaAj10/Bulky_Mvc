f75abad1a3d998de9f31b2d725b1bb33cef20e0b
Omar <omar.m10@hotmail.com>
2024-05-08 14:32:43 +0200

    Implement crud and views for product

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -15,19 +15,19 @@ public class Product
     public string Author { get; set; }
     [Required]
     [Display(Name = "List Price")]
-    [Range(1, 100)]
+    [Range(1, 1000)]
     public double ListPrice { get; set; }
 
     [Display(Name = "List Price 1-50")]
-    [Range(1, 100)]
+    [Range(1, 1000)]
     public double Price { get; set; }
 
     [Display(Name = "List Price 50+")]
-    [Range(1, 100)]
+    [Range(1, 1000)]
     public double Price50 { get; set; }
 
     [Display(Name = "List Price 100+")]
-    [Range(1, 100)]
+    [Range(1, 1000)]
     public double Price100 { get; set; }
 
 }
