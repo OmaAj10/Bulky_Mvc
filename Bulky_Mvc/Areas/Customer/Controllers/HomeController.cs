79cd78d705cdf3ccf218006e629df0250a3540fa
Omar <omar.m10@hotmail.com>
2024-05-10 17:25:54 +0200

    Fix home page and details UI

--- Bulky_Mvc/Areas/Customer/Controllers/HomeController.cs
+++ Bulky_Mvc/Areas/Customer/Controllers/HomeController.cs
@@ -1,22 +1,32 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Bulky.Models;
+using Bulky.DataAccess.Repository.IRepository;
 
 namespace Bulky_Mvc.Area.Customer.Controllers;
 
 [Area("Customer")]
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(IUnitOfWork unitOfWork)
     {
-        _logger = logger;
+        _unitOfWork = unitOfWork;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+
+        return View(productList);
+    }
+
+    public IActionResult Details(int productId)
+    {
+        var getProduct = _unitOfWork.Product.Get(p => p.Id == productId, includeProperties: "Category");
+
+        return View(getProduct);
     }
 
     public IActionResult Privacy()
