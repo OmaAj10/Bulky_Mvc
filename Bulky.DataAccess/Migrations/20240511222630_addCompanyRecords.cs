a0ba5f12eeca77f1276f4c0b279595280c7f5b8e
Omar <omar.m10@hotmail.com>
2024-05-12 00:46:11 +0200

    Company migrations

---
+++ Bulky.DataAccess/Migrations/20240511222630_addCompanyRecords.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCompanyRecords : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "City", "Name", "PhoneNumber", "PostalCode", "State", "StreetAddress" },
+                values: new object[,]
+                {
+                    { 1, "Tech city", "Tech", "121212121212", "121212", "TH", "124 tech st" },
+                    { 2, "Code city", "Tech 2", "121212121212", "121212", "CD", "Code st" },
+                    { 3, "IT city", "Tech 3", "121212121212", "121212", "It", "It" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
