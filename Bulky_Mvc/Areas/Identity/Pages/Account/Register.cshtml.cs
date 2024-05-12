ff7612ade3a90476bd439d3ed9590303a42ca5e6
Omar <omar.m10@hotmail.com>
2024-05-12 16:32:57 +0200

    add migration CompanyToUser and Register Company to user

--- Bulky_Mvc/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Bulky_Mvc/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -10,10 +10,10 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
 using Bulky.Utility;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -21,7 +21,6 @@ using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
-using Microsoft.Extensions.Logging;
 
 namespace Bulky_Mvc.Areas.Identity.Pages.Account
 {
@@ -34,6 +33,7 @@ namespace Bulky_Mvc.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<IdentityUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly IUnitOfWork _unitOfWork;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
@@ -41,8 +41,10 @@ namespace Bulky_Mvc.Areas.Identity.Pages.Account
             IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
             _roleManager = roleManager;
             _userManager = userManager;
             _userStore = userStore;
@@ -115,6 +117,8 @@ namespace Bulky_Mvc.Areas.Identity.Pages.Account
             public string? State { get; set; }
             public string? PostalCode { get; set; }
             public string? PhoneNumber { get; set; }
+            public int? CompanyId { get; set; }
+            public IEnumerable<SelectListItem> CompanyList { get; set; }
         }
         public async Task OnGetAsync(string returnUrl = null)
         {
@@ -132,6 +136,11 @@ namespace Bulky_Mvc.Areas.Identity.Pages.Account
                 {
                     Text = i,
                     Value = i
+                })),
+                CompanyList = _unitOfWork.Company.GetAll().Select((i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
                 }))
             };
             
@@ -156,6 +165,12 @@ namespace Bulky_Mvc.Areas.Identity.Pages.Account
                 user.State = Input.State;
                 user.PostalCode = Input.PostalCode;
                 user.PhoneNumber = Input.PhoneNumber;
+
+                if (Input.Role == StaticDetails.Role_Company)
+                {
+                    user.CompanyId = Input.CompanyId;
+                }
+                
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
